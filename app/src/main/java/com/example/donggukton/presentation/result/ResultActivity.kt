71f661c437fdf89cab9d5e88d77a5fedf35b70d4
l2zh <dlwnsgml0109@naver.com>
2023-12-18 19:40:07 +0900

    [feat] #11 결과 뷰 작업(뷰 페이저 이전까지)

---
+++ app/src/main/java/com/example/donggukton/presentation/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.donggukton.presentation.result
+
+import android.os.Bundle
+import com.example.donggukton.R
+import com.example.donggukton.databinding.ActivityResultBinding
+import com.example.donggukton.util.binding.BindingActivity
+
+class ResultActivity : BindingActivity<ActivityResultBinding>(R.layout.activity_result) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
