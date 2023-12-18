574092b0c62b8d7aeb8a3af3b2ee63c687cb21a8
Dani43 <eldms0406@naver.com>
2023-12-18 16:15:31 +0900

    [feat] #4 온보딩 뷰 구현

---
+++ app/src/main/java/com/example/donggukton/presentation/OnBoardingAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.donggukton.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.donggukton.databinding.ItemOnboardingBinding
+import com.example.donggukton.presentation.type.OnBoardingViewType
+
+class OnBoardingAdapter : RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+    private val onBoardingList = OnBoardingViewType.values()
+
+    class OnBoardingViewHolder(private val binding: ItemOnboardingBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(onboarding: OnBoardingViewType) {
+            binding.ivOnboardingImg.setImageDrawable(
+                binding.root.context.getDrawable(onboarding.imageRes)
+            )
+            binding.tvOnboarding.text = binding.root.context.getString(onboarding.desRes)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder =
+        OnBoardingViewHolder(
+            ItemOnboardingBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount() = onBoardingList.size
+
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
+        holder.bind(onBoardingList[position])
+    }
+}
