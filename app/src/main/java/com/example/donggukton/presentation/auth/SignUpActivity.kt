9939453ee365717f31b3861873710d7c78a8c103
Dani43 <eldms0406@naver.com>
2023-12-19 15:36:24 +0900

    [feat] #15 회원가입 뷰 디자인 및 액티비티 추가

---
+++ app/src/main/java/com/example/donggukton/presentation/auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.donggukton.presentation.auth
+
+import android.os.Bundle
+import com.example.donggukton.R
+import com.example.donggukton.databinding.ActivitySignUpBinding
+import com.example.donggukton.util.binding.BindingActivity
+
+class SignUpActivity: BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
